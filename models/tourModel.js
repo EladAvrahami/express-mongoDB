23e9e6152c035a907b298833f3f6b1e38e686bae
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2022-08-20 21:05:10 +0300

    tourModel -add GeoJSON create an embedded documents       type: {

--- models/tourModel.js
+++ models/tourModel.js
@@ -77,29 +77,38 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+
+    startLocation: {
+      //GeoJSON -create an embedded documents
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
   }
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
